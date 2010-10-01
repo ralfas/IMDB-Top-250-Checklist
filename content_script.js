9e4c50cd1695c5e5f304f5ff33ee67a1b969d5be
Ralfas <ralfas@gmail.com>
2010-10-01 20:13:26 +0100

    watched count

--- content_script.js
+++ content_script.js
@@ -2,9 +2,17 @@ var MM = {
 	cache : {},
 	fn : {}
 };
+
 MM.fn = {
+	insertRow : function() {
+		var html = '<tr><td colspan=5 class="MM-right"><span id="MM-viewed"></span></td></tr>';
+
+		MM.cache.tableBody.append(html);
+		MM.cache.total = MM.cache.tableBody.find('#MM-viewed');
+	},
+
 	insertBoxes : function() {
-		var rowNum = 1, eachHandler;
+		var rowNum = 1, eachHandler, viewed = 0;
 
 		eachHandler = function() {
 			var headingHTML, boxHTML, checkedBoxHTML, row = $(this), clickHandler, itemID;
@@ -15,29 +23,42 @@ MM.fn = {
 
 			if (rowNum === 1) {
 				row.append(headingHTML);
-			} else {
+			} else if (rowNum <= 251) {
 				itemID = row.find('a').attr('href').replace('/title/', '')
 						.slice(0, -1);
 
-				localStorage.getItem(itemID) === 'true' ? row
-						.append(checkedBoxHTML) : row.append(boxHTML);
+				if (localStorage.getItem(itemID) === 'true') {
+					row.append(checkedBoxHTML);
+					viewed += 1;
+				} else {
+					row.append(boxHTML);
+				}
 
 				clickHandler = function() {
-					$(this).is(':checked') ? localStorage.setItem(itemID,
-							'true') : localStorage.removeItem(itemID);
+					if ($(this).is(':checked')) {
+						localStorage.setItem(itemID, 'true');
+						viewed += 1;
+					} else {
+						localStorage.removeItem(itemID);
+						viewed -= 1;
+					}
+					MM.cache.total.html(viewed + ' of 250');
 				};
-
 				row.find('input[type=checkbox]').click(clickHandler);
 			}
 			rowNum += 1;
 		};
+		MM.cache.tableBody.find('tr').each(eachHandler);
 
-		MM.cache.main.find('table tbody tr').each(eachHandler);
+		MM.cache.total.html(viewed + ' of 250');
 	},
 
 	init : function() {
 		MM.cache.main = $('#main');
+		MM.cache.tableBody = MM.cache.main.find('table tbody');
+		MM.fn.insertRow();
 		MM.fn.insertBoxes();
 	}
 };
+
 MM.fn.init();
